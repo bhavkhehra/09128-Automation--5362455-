faa1145ef3d61c1177d1ba4a5db8454ffe5d33ed
bhavkhehra <88630785+bhavkhehra@users.noreply.github.com>
2022-02-05 14:44:46 -0500

    update pro_1.py

--- pro_1.py
+++ pro_1.py
@@ -3,23 +3,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 import unittest
+import pytest
 
 
-def expected(test1):
-    webdriver.Chrome("C:\\Users\\HP\\Downloads\\chromedriver_win32\\chromedriver.exe")
+def test():
+   driver = webdriver.Chrome("C:\\Users\\HP\\Downloads\\chromedriver_win32\\chromedriver.exe")
 
-    driver.get('http://google.com')
+   driver.get('http://google.com')
+   driver.find_element(By.NAME, "q").send_keys("ferrrari")
+   expected = "displaying at least 10 search results"
 
-    driver.find_element(By.NAME, "q").send_keys("ferrrari").click()
-    expected = string("displaying at least 10 search results")
+   actual = "displaying at least 8 search results"
 
-def actual(test2):
-     webdriver.Chrome("C:\\Users\\HP\\Downloads\\chromedriver_win32\\chromedriver.exe")
-
-
-     driver.get('http://google.com')
-
-     driver.find_element(By.CLASS_NAME, "OBMEnb")
-     actual = string("displaying at least 10 search results")
-
-assert expected(test1="displaying at least 10 search results") == actual(test2="displaying at least 10 search results")
+   assert expected != actual
